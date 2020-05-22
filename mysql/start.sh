d993209ee3288852ed1cd5660833ebaeba8fd3b2
Alex Xu <xuyc@sina.com>
2020-05-22 13:08:44 +0800

    modified:   mysql/start.sh 	new file:   nginx/cleanup.sh 	modified:   nginx/deployment.yaml 	new file:   nginx/start.sh

--- mysql/start.sh
+++ mysql/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 kubectl create -f mysql-secret.yaml 
-kubectl create -f mysql-pv-old.yaml
 kubectl create -f mysql-deployment.yaml
 kubectl create -f mysql-service.yaml 
+kubectl create -f mysql-pv-old.yaml
+
---
+++ nginx/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+kubectl create -f deployment.yaml 
+
